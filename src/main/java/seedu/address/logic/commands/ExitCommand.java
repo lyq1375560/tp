e2aa6d8d1ee229a74f72bb8b2122c1741d1e6945
Sureshkumar Sheasu <sheasusheasu@gmail.com>
2026-03-15 00:20:54 +0800

    Show customer count in exit message

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -8,12 +8,15 @@ import seedu.address.model.Model;
 public class ExitCommand extends Command {
 
     public static final String COMMAND_WORD = "exit";
-
-    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
+    public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT =
+            "Goodbye! Exiting ClientEase. You have %d customer(s) saved.";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
+        int count = model.getFilteredPersonList().size();
+        String message = String.format(MESSAGE_EXIT_ACKNOWLEDGEMENT, count);
+
+        return new CommandResult(message, false, true);
     }
 
 }
